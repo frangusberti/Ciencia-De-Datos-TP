b23e2dc38e3de61e40e3ab58429e9d88702c8a46
mrisso-cmd <mrisso@fi.uba.ar>
2026-05-01 12:53:59 -0300

    hice un coso de prueba

--- massi.py
+++ massi.py
@@ -4,4 +4,3 @@ from datetime import datetime
 df = pd.read_csv("/Users/maximarissop/Desktop/Ciencia de Datos/Ciencia-De-Datos-TP/customer_behavior_dataset.csv")
 nulos = df.isnull().sum()
 print(nulos[nulos > 0])
-print(df.describe())
