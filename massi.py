b43b351393873c4c2f9d69e161ac9d7a3ee20fb8
mrisso-cmd <mrisso@fi.uba.ar>
2026-05-01 12:40:03 -0300

    hice mi rama

---
+++ massi.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+df = pd.read_csv("/Users/maximarissop/Desktop/Ciencia de Datos/Ciencia-De-Datos-TP/customer_behavior_dataset.csv")
+nulos = df.isnull().sum()
+print(nulos[nulos > 0])
+print(df.describe())
