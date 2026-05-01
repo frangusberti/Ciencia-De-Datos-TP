1742c261c1030e51813878d2522d2134d522ede7
mrisso-cmd <mrisso@fi.uba.ar>
2026-05-01 17:50:35 -0300

    intento de detectar valores no debidos

--- massi.py
+++ massi.py
@@ -4,3 +4,10 @@ from datetime import datetime
 df = pd.read_csv("/Users/maximarissop/Desktop/Ciencia de Datos/Ciencia-De-Datos-TP/customer_behavior_dataset.csv")
 nulos = df.isnull().sum()
 print(nulos[nulos > 0])
+#hasta ahora programe para que cuente valores que faltan en por columna, podria ver el porcentaje de valores nulos si quiero pero me parece medio innecesario
+#veo que hago con esos valores nulos pq hay algunas categorias que tienen muchos valores nulos y otras no tantos
+#me trato de sacar de encima valores extraños (ejs: años y sueldos negativos)
+print(df.describe())
+#se detectan esos valores atipicos observando el minimo y el maxixmo valor de las columnas
+#Pueden emplearse conceptos de Probabilidad y Estadistica (Teorema Central del Limite) para ver si los valores de las columnas pertenecen o no al rango especificado
+#pruebo detectar valores atipicos para la columna de year_birth, tengo que definir que son los outliers
